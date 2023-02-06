e20b98cec5d681e4f33ed2d54d59a7547ad61a7d
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-06 21:25:06 +0700

    init

--- KonsepReact/Deklaratif.js
+++ KonsepReact/Deklaratif.js
@@ -1,4 +1,18 @@
-const names = ['Asep', 'Alex', 'Bagus', 'Cika', 'Doni'];
-const uppercaseNames = names.map((name) => name.toUpperCase());
- 
-console.log(uppercaseNames);
+// const names = ['Asep', 'Alex', 'Bagus', 'Cika', 'Doni'];
+// const uppercaseNames = names.map((name) => name.toUpperCase());
+
+// console.log(uppercaseNames);
+
+function Contacts() {
+    const names = ['Asep', 'Alex', 'Bagus', 'Cika', 'Doni'];
+
+    return (
+        <ol className='contacts'>
+            {names.map((name) => <li>{name}</li>)}
+        </ol>
+    );
+}
+
+export default Contacts;
+
+<button onClick={callContact}>Call Contact</button>
