d9dca391b30841899113b6e57a13b915da60b434
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-06 21:19:16 +0700

    init

---
+++ KonsepReact/imperatif.js
@@ -0,0 +1,8 @@
+const names = ['Asep', 'Alex', 'Bagus', 'Cika', 'Doni'];
+const uppercaseNames = [];
+
+for (let i = 0; i < names.length; i++) {
+  uppercaseNames[i] = names[i].toUpperCase();
+}
+
+console.log(uppercaseNames);
