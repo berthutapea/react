edb52b8f5d592ff71d8e6ca31fb6ac5cd58c37a5
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-07 00:18:33 +0700

    init

---
+++ ReactUIComponent/JSX/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+// Teknik modularisasi
+import DicodingLogo from './dicoding-header-logo.png'
+
+const element = (
+    <div>
+        <h1>Biodata Perusahaan</h1>
+        <ul>
+            <li>Nama: Dicoding Indonesia</li>
+            <li>Bidang: Education</li>
+            <li>Tagline: Decode Ideas, Discover Potential.</li>
+            {/* <img src="./img/dicoding-header-logo.png" alt="Dicoding Logo" /> */}
+            {/* // Teknik modularisasi */}
+            <img src={DicodingLogo} alt="Dicoding Logo" />
+        </ul>
+    </div>
+);
+
+const root = createRoot(document.getElementById('root'));
+root.render(element);
