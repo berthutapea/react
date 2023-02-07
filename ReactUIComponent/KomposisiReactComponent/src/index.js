9a4d8e6efefb7806f52b399152c54247ac0043a3
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-07 22:06:15 +0700

    init

--- ReactUIComponent/KomposisiReactComponent/src/index.js
+++ ReactUIComponent/KomposisiReactComponent/src/index.js
@@ -2,81 +2,81 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 function Button({ link }) {
-  return <a href={link}>Find out more</a>;
+    return <a href={link}>Find out more</a>;
 }
 
 function CardHeader({ image, category }) {
-  return (
-    <header>
-      <h4>{category}</h4>
-      <img src={image} alt="" />
-    </header>
-  );
+    return (
+        <header>
+            <h4>{category}</h4>
+            <img src={image} alt="" />
+        </header>
+    );
 }
 
 function CardBody({ date, title, content, link }) {
-  return (
-    <div>
-      <p>{date}</p>
-      <h2>{title}</h2>
-      <p>{content}</p>
-      <Button link={link} />
-    </div>
-  );
+    return (
+        <div>
+            <p>{date}</p>
+            <h2>{title}</h2>
+            <p>{content}</p>
+            <Button link={link} />
+        </div>
+    );
 }
 
 function Card({ image, category, date, title, content, link }) {
-  return (
-    <article>
-      <CardHeader image={image} category={category} />
-      <CardBody date={date} title={title} content={content} link={link} />
-    </article>
-  );
+    return (
+        <article>
+            <CardHeader image={image} category={category} />
+            <CardBody date={date} title={title} content={content} link={link} />
+        </article>
+    );
 }
 
 function Header({ title, subtitle }) {
-  return (
-    <header>
-      <h1>{title}</h1>
-      <p>{subtitle}</p>
-    </header>
-  );
+    return (
+        <header>
+            <h1>{title}</h1>
+            <p>{subtitle}</p>
+        </header>
+    );
 }
 
 function News() {
-  // data news
-  const someNews = [
-    {
-      title: 'CNN Acuire BEME',
-      date: 'March 20 2022',
-      content: "CNN purchased Casey Neistat's Beme app for $25million.",
-      image: 'https://source.unsplash.com/user/erondu/600x400',
-      category: 'News',
-      link: '#'
-    },
-    {
-      title: 'React and the WP-API',
-      date: 'March 19 2022',
-      content: 'The first ever decoupled starter theme for React & the WP-API.',
-      image: 'https://source.unsplash.com/user/ilyapavlov/600x400',
-      category: 'News',
-      link: '#'
-    },
-    {
-      title: 'Nomad Lifestyle',
-      date: 'March 19 2022',
-      content: 'Learn our tips and tricks on living a nomadic lifestyle.',
-      image: 'https://source.unsplash.com/user/erondu/600x400',
-      category: 'Travel',
-      link: '#'
-    }
-  ];
+    // data news
+    const someNews = [
+        {
+            title: 'CNN Acuire BEME',
+            date: 'March 20 2022',
+            content: "CNN purchased Casey Neistat's Beme app for $25million.",
+            image: 'https://source.unsplash.com/user/erondu/600x400',
+            category: 'News',
+            link: '#'
+        },
+        {
+            title: 'React and the WP-API',
+            date: 'March 19 2022',
+            content: 'The first ever decoupled starter theme for React & the WP-API.',
+            image: 'https://source.unsplash.com/user/ilyapavlov/600x400',
+            category: 'News',
+            link: '#'
+        },
+        {
+            title: 'Nomad Lifestyle',
+            date: 'March 19 2022',
+            content: 'Learn our tips and tricks on living a nomadic lifestyle.',
+            image: 'https://source.unsplash.com/user/erondu/600x400',
+            category: 'Travel',
+            link: '#'
+        }
+    ];
 
-  // TODO: selesaikan component-nya
-  return (
-    <div>
-      <Header title="Latest News" subtitle="Covering March & April 2022" />
-      <Card
+    // TODO: selesaikan component-nya
+    return (
+        <div>
+            <Header title="Latest News" subtitle="Covering March & April 2022" />
+            {/* <Card
         title={someNews[0].title}
         date={someNews[0].date}
         content={someNews[0].content}
@@ -99,9 +99,13 @@ function News() {
         image={someNews[2].image}
         category={someNews[2].category}
         link={someNews[2].link}
-      />
-    </div>
-  );
+      /> */}
+
+            <Card {...someNews[0]} />
+            <Card {...someNews[1]} />
+            <Card {...someNews[2]} />
+        </div>
+    );
 }
 
 const root = createRoot(document.getElementById('root'));
