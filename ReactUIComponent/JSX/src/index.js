472c9f12ef0429abf55ea9598647543c678a17ff
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-07 21:06:13 +0700

    init

--- ReactUIComponent/JSX/src/index.js
+++ ReactUIComponent/JSX/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 // Teknik modularisasi
-import DicodingLogo from './dicoding-header-logo.png'
+// import DicodingLogo from './dicoding-header-logo.png'
 
 const element = (
     <div>
