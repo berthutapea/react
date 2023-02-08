03a5680c3bafd06f9149d25db31b6e76a1867d7f
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-08 21:20:38 +0700

    init

--- react-projects/contacts-app/src/ContactItemImage.js
+++ react-projects/contacts-app/src/ContactItemImage.js
@@ -1,11 +1,11 @@
-// import React from 'react';
+import React from 'react';
 
-// function ContactItemImage({ imageUrl }) {
-//     return (
-//         <div className="contact-item__image">
-//             <img src={imageUrl} alt="contact avatar" />
-//         </div>
-//     );
-// }
+function ContactItemImage({ imageUrl }) {
+    return (
+        <div className="contact-item__image">
+            <img src={imageUrl} alt="contact avatar" />
+        </div>
+    );
+}
 
-// export default ContactItemImage;
+export default ContactItemImage;
