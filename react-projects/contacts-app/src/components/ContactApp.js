2701cf098b936faed1a151c666a5fc8bb8652e13
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-08 23:19:54 +0700

    init

--- react-projects/contacts-app/src/components/ContactApp.js
+++ react-projects/contacts-app/src/components/ContactApp.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ContactList from './ContactList';
-import { getData } from '../utils/data';
+// import React from 'react';
+// import ContactList from './ContactList';
+// import { getData } from '../utils/data';
 
-function ContactApp() {
-    const contacts = getData();
+// function ContactApp() {
+//     const contacts = getData();
 
-    return (
-        <div className="contact-app">
-            <h1>Daftar Kontak</h1>
-            <ContactList contacts={contacts} />
-        </div>
-    );
-}
+//     return (
+//         <div className="contact-app">
+//             <h1>Daftar Kontak</h1>
+//             <ContactList contacts={contacts} />
+//         </div>
+//     );
+// }
 
 
-export default ContactApp;
+// export default ContactApp;
