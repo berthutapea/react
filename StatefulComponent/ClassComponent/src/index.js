7f8c6bba6aa08cc04a44537039071a3fac262739
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-09 00:05:47 +0700

    init

---
+++ StatefulComponent/ClassComponent/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log('Component created!');
+    }
+
+    render() {
+        return <p>Hello, {this.props.name}!</p>;
+    }
+}
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+    <div>
+        <MyComponent name="Dicoding" />
+        <MyComponent name="John" />
+        <MyComponent name="Doe" />
+    </div>
+);
