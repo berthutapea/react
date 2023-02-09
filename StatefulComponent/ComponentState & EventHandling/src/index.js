abbea2eac50b27ea657fa3544a127a941720d9fe
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-10 00:39:14 +0700

    init

--- StatefulComponent/ComponentState & EventHandling/src/index.js
+++ StatefulComponent/ComponentState & EventHandling/src/index.js
@@ -2,29 +2,37 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 function CounterDisplay({ count }) {
-  if (count === 0) {
+    if (count === 0) {
+        return <p>{count}</p>;
+    }
+
+    if (count % 5 === 0 && count % 7 === 0) {
+        return <p>FizzBuzz</p>;
+    }
+
+    if (count % 5 === 0) {
+        return <p>Fizz</p>;
+    }
+
+    if (count % 7 === 0) {
+        return <p>Buzz</p>;
+    }
+
     return <p>{count}</p>;
-  }
-
-  if (count % 5 === 0 && count % 7 === 0) {
-    return <p>FizzBuzz</p>;
-  }
-
-  if (count % 5 === 0) {
-    return <p>Fizz</p>;
-  }
-
-  if (count % 7 === 0) {
-    return <p>Buzz</p>;
-  }
-
-  return <p>{count}</p>;
 }
 
 function IncreaseButton({ increase }) {
     return (
-      <div>
-        <button onClick={increase}>+ increase</button>
-      </div>
+        <div>
+            <button onClick={increase}>+ increase</button>
+        </div>
     );
-  }
+}
+
+function ResetButton({ reset }) {
+    return (
+        <div>
+            <button onClick={reset}>- reset</button>
+        </div>
+    );
+}
