19cf936e01c539d30994fd006921da6e1290c7c4
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-09 23:15:30 +0700

    init

--- react-projects/contacts-app/src/index.js
+++ react-projects/contacts-app/src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import ContactApp from './components/ContactApp';
+// import React from 'react';
+// import { createRoot } from 'react-dom/client';
+// import ContactApp from './components/ContactApp';
 
-// styling
-import './styles/style.css';
+// // styling
+// import './styles/style.css';
 
-const root = createRoot(document.getElementById('root'));
-root.render(<ContactApp />);
+// const root = createRoot(document.getElementById('root'));
+// root.render(<ContactApp />);
