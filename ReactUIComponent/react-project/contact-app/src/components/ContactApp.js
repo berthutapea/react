a2850a3aa834c8ea10a87148c6456c0bd13dcdb5
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-10 04:35:52 +0700

    init

--- ReactUIComponent/react-project/contact-app/src/components/ContactApp.js
+++ ReactUIComponent/react-project/contact-app/src/components/ContactApp.js
@@ -1,51 +1,51 @@
-// import React from 'react';
-// import ContactList from './ContactList';
-// import { getData } from '../utils/data';
-// import ContactInput from './ContactInput';
+import React from 'react';
+import ContactList from './ContactList';
+import { getData } from '../utils/data';
+import ContactInput from './ContactInput';
 
-// class ContactApp extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             contacts: getData(),
-//         }
+class ContactApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            contacts: getData(),
+        }
 
-//         this.onDeleteHandler = this.onDeleteHandler.bind(this);
-//         this.onAddContactHandler = this.onAddContactHandler.bind(this);
-//     }
+        this.onDeleteHandler = this.onDeleteHandler.bind(this);
+        this.onAddContactHandler = this.onAddContactHandler.bind(this);
+    }
 
-//     onDeleteHandler(id) {
-//         const contacts = this.state.contacts.filter(contact => contact.id !== id);
-//         this.setState({ contacts });
-//     }
+    onDeleteHandler(id) {
+        const contacts = this.state.contacts.filter(contact => contact.id !== id);
+        this.setState({ contacts });
+    }
 
-//     onAddContactHandler({ name, tag }) {
-//         this.setState((prevState) => {
-//             return {
-//                 contacts: [
-//                     ...prevState.contacts,
-//                     {
-//                         id: +new Date(),
-//                         name,
-//                         tag,
-//                         imageUrl: '/images/default.jpg',
-//                     }
-//                 ]
-//             }
-//         });
-//     }
+    onAddContactHandler({ name, tag }) {
+        this.setState((prevState) => {
+            return {
+                contacts: [
+                    ...prevState.contacts,
+                    {
+                        id: +new Date(),
+                        name,
+                        tag,
+                        imageUrl: '/images/default.jpg',
+                    }
+                ]
+            }
+        });
+    }
 
-//     render() {
-//         return (
-//             <div className="contact-app">
-//                 <h1>Aplikasi Kontak</h1>
-//                 <h2>Tambah Kontak</h2>
-//                 <ContactInput addContact={this.onAddContactHandler} />
-//                 <h2>Daftar Kontak</h2>
-//                 <ContactList contacts={this.state.contacts} onDelete={this.onDeleteHandler} />
-//             </div>
-//         );
-//     }
-// }
+    render() {
+        return (
+            <div className="contact-app">
+                <h1>Aplikasi Kontak</h1>
+                <h2>Tambah Kontak</h2>
+                <ContactInput addContact={this.onAddContactHandler} />
+                <h2>Daftar Kontak</h2>
+                <ContactList contacts={this.state.contacts} onDelete={this.onDeleteHandler} />
+            </div>
+        );
+    }
+}
 
-// export default ContactApp;
+export default ContactApp;
