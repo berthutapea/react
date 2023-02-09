6ad55bb5e81e5fd816c834b731a7ac1520650ee3
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-09 23:37:05 +0700

    init

--- react-projects/contacts-app/src/components/ContactList.js
+++ react-projects/contacts-app/src/components/ContactList.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ContactItem from './ContactItem';
+// import React from 'react';
+// import ContactItem from './ContactItem';
 
-function ContactList({ contacts }) {
-    return (
-        <div className="contact-list">
-            {
-                contacts.map((contact) => (
-                    <ContactItem key={contact.id} {...contact} />
-                ))
-            }
-        </div>
-    );
-}
+// function ContactList({ contacts }) {
+//     return (
+//         <div className="contact-list">
+//             {
+//                 contacts.map((contact) => (
+//                     <ContactItem key={contact.id} {...contact} />
+//                 ))
+//             }
+//         </div>
+//     );
+// }
 
-export default ContactList;
+// export default ContactList;
