53c1222360c4d5f63810165fe4ac8f99a614d64d
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-10 00:41:11 +0700

    init

--- StatefulComponent/ComponentState & EventHandling/src/index.js
+++ StatefulComponent/ComponentState & EventHandling/src/index.js
@@ -2,37 +2,81 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 function CounterDisplay({ count }) {
-    if (count === 0) {
-        return <p>{count}</p>;
-    }
-
-    if (count % 5 === 0 && count % 7 === 0) {
-        return <p>FizzBuzz</p>;
-    }
-
-    if (count % 5 === 0) {
-        return <p>Fizz</p>;
-    }
-
-    if (count % 7 === 0) {
-        return <p>Buzz</p>;
-    }
-
+  if (count === 0) {
     return <p>{count}</p>;
+  }
+
+  if (count % 5 === 0 && count % 7 === 0) {
+    return <p>FizzBuzz</p>;
+  }
+
+  if (count % 5 === 0) {
+    return <p>Fizz</p>;
+  }
+
+  if (count % 7 === 0) {
+    return <p>Buzz</p>;
+  }
+
+  return <p>{count}</p>;
 }
 
 function IncreaseButton({ increase }) {
-    return (
-        <div>
-            <button onClick={increase}>+ increase</button>
-        </div>
-    );
+  return (
+    <div>
+      <button onClick={increase}>+ increase</button>
+    </div>
+  );
 }
 
 function ResetButton({ reset }) {
-    return (
-        <div>
-            <button onClick={reset}>- reset</button>
-        </div>
-    );
+  return (
+    <div>
+      <button onClick={reset}>- reset</button>
+    </div>
+  );
 }
+
+class CounterApp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // inisialisasi nilai count di dalam state
+    this.state = {
+      count: 0
+    };
+
+    // binding event handler
+    this.onIncreaseEventHandler = this.onIncreaseEventHandler.bind(this);
+    this.onResetEventHandler = this.onResetEventHandler.bind(this);
+  }
+
+  onIncreaseEventHandler() {
+    this.setState((previousState) => {
+      return {
+        count: previousState.count + 1
+      };
+    });
+  }
+
+  onResetEventHandler() {
+    this.setState(() => {
+      return {
+        count: 0
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <IncreaseButton increase={this.onIncreaseEventHandler} />
+        <CounterDisplay count={this.state.count} />
+        <ResetButton reset={this.onResetEventHandler} />
+      </div>
+    );
+  }
+}
+
+const root = createRoot(document.getElementById('root'));
+root.render(<CounterApp />);
