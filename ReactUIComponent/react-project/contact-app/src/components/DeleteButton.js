204f2f4157ccd3d58007fde491db3c64b5b5325a
Gilbert Hutapea <111676859+berthutapea@users.noreply.github.com>
2023-02-10 04:50:57 +0700

    Delete ReactUIComponent/react-project/contact-app directory

--- ReactUIComponent/react-project/contact-app/src/components/DeleteButton.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function DeleteButton({ id, onDelete }) {
-    return <button className='contact-item__delete' onClick={() => onDelete(id)}>x</button>
-}
-
-export default DeleteButton;
--- ReactUIComponent/react-project/contact-app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import ContactApp from './components/ContactApp';
-
-// styling
-import './styles/style.css';
-
-const root = createRoot(document.getElementById('root'));
-root.render(<ContactApp />);
