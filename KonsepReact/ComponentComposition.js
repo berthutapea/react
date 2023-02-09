7d56a0be9bdcb7d62eb5ebd9236d66b8a8ac99a6
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-10 02:19:46 +0700

    init

--- KonsepReact/ComponentComposition.js
+++ KonsepReact/ComponentComposition.js
@@ -1,21 +1,21 @@
-// function ProfilePicture({ userId }) {
-//     return (
-//         <img src={`https://avatars.githubusercontent.com/u/${userId}`} />
-//     );
-// }
+function ProfilePicture({ userId }) {
+    return (
+        <img src={`https://avatars.githubusercontent.com/u/${userId}`} />
+    );
+}
 
-// function ProfileLink({ username }) {
-//     return (
-//         <a href={`https://github.com/${username}`} />
-//     )
-// }
+function ProfileLink({ username }) {
+    return (
+        <a href={`https://github.com/${username}`} />
+    )
+}
 
 
-// function GithubInfo({ username, userId }) {
-//     return (
-//         <div className='github-info'>
-//             <ProfilePicture userId={userId} />
-//             <ProfileLink username={username} />
-//         </div>
-//     )
-// }
+function GithubInfo({ username, userId }) {
+    return (
+        <div className='github-info'>
+            <ProfilePicture userId={userId} />
+            <ProfileLink username={username} />
+        </div>
+    )
+}
