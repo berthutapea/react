c7c7239e88db26f27bfcdf61658b6840f9605868
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-13 22:42:40 +0700

    React Router

--- ReactRouter/Single-Page-App-Dengan-React-Router/src/index.js
+++ ReactRouter/Single-Page-App-Dengan-React-Router/src/index.js
@@ -1,49 +1,49 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
+// import React from 'react';
+// import { createRoot } from 'react-dom/client';
+// import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
 
-function HomePage() {
-    return <p>This is Homepage</p>;
-}
-
-// function AboutPage() {
-//     return <p>This is About page</p>;
+// function HomePage() {
+//     return <p>This is Homepage</p>;
 // }
 
-// function FAQPage() {
-//     return <p>This is FAQ page</p>;
+// // function AboutPage() {
+// //     return <p>This is About page</p>;
+// // }
+
+// // function FAQPage() {
+// //     return <p>This is FAQ page</p>;
+// // }
+
+// function App() {
+//     return (
+//         <>
+//             <header>
+//                 <ul>
+//                     <li>
+//                         <Link to="/">Home</Link>
+//                     </li>
+//                     <li>
+//                         <Link to="/about">About</Link>
+//                     </li>
+//                     <li>
+//                         <Link to="/faq">FAQ</Link>
+//                     </li>
+//                 </ul>
+//             </header>
+//             <main>
+//                 <Routes>
+//                     <Route path="/" element={<HomePage />} />
+//                     <Route path="/about" element={<AboutPage />} />
+//                     <Route path="/faq" element={<FAQPage />} />
+//                 </Routes>
+//             </main>
+//         </>
+//     );
 // }
 
-function App() {
-    return (
-        <>
-            <header>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">About</Link>
-                    </li>
-                    <li>
-                        <Link to="/faq">FAQ</Link>
-                    </li>
-                </ul>
-            </header>
-            <main>
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/about" element={<AboutPage />} />
-                    <Route path="/faq" element={<FAQPage />} />
-                </Routes>
-            </main>
-        </>
-    );
-}
-
-const root = createRoot(document.getElementById('root'));
-root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-);
+// const root = createRoot(document.getElementById('root'));
+// root.render(
+//     <BrowserRouter>
+//         <App />
+//     </BrowserRouter>
+// );
