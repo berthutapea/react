8b89998124f13d2a7c828cb0a483932d5a706086
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-13 22:43:16 +0700

    React Router

--- ReactRouter/Single-Page-App-Dengan-React-Router/src/index.js
+++ ReactRouter/Single-Page-App-Dengan-React-Router/src/index.js
@@ -6,13 +6,13 @@
 //     return <p>This is Homepage</p>;
 // }
 
-// // function AboutPage() {
-// //     return <p>This is About page</p>;
-// // }
+// function AboutPage() {
+//     return <p>This is About page</p>;
+// }
 
-// // function FAQPage() {
-// //     return <p>This is FAQ page</p>;
-// // }
+// function FAQPage() {
+//     return <p>This is FAQ page</p>;
+// }
 
 // function App() {
 //     return (
