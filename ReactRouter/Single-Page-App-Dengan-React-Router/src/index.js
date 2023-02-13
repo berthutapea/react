75b06839846dc097e3e55a882c9d51613c64be6a
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-13 22:24:21 +0700

    React Router

--- ReactRouter/Single-Page-App-Dengan-React-Router/src/index.js
+++ ReactRouter/Single-Page-App-Dengan-React-Router/src/index.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
+
 function HomePage() {
     return <p>This is Homepage</p>;
 }
@@ -9,3 +13,37 @@ function AboutPage() {
 function FAQPage() {
     return <p>This is FAQ page</p>;
 }
+
+function App() {
+    return (
+        <>
+            <header>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/faq">FAQ</Link>
+                    </li>
+                </ul>
+            </header>
+            <main>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/about" element={<AboutPage />} />
+                    <Route path="/faq" element={<FAQPage />} />
+                </Routes>
+            </main>
+        </>
+    );
+}
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
