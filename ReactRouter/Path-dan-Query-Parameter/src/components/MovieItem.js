cc3a900fab7abd0adceb7c36c7a9d92b3ebe6d5e
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-14 23:29:36 +0700

    Path & Query Parameter

--- ReactRouter/Path-dan-Query-Parameter/src/components/MovieItem.js
+++ ReactRouter/Path-dan-Query-Parameter/src/components/MovieItem.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-function MovieItem({ id, title, backdropPath, overview }) {
-    return (
-        <article>
-            <img src={backdropPath} alt={title} />
-            <h3>
-                <Link to={`/movies/${id}`}>{title}</Link>
-            </h3>
-            <p>{overview}</p>
-        </article>
-    );
-}
+// function MovieItem({ id, title, backdropPath, overview }) {
+//     return (
+//         <article>
+//             <img src={backdropPath} alt={title} />
+//             <h3>
+//                 <Link to={`/movies/${id}`}>{title}</Link>
+//             </h3>
+//             <p>{overview}</p>
+//         </article>
+//     );
+// }
 
 MovieItem.propTypes = {
     id: PropTypes.number.isRequired,
