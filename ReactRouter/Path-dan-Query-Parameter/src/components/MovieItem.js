517e156ead7910ba72b77cfd4307e42caba64bd1
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-14 21:53:30 +0700

    Path & Query Parameter

---
+++ ReactRouter/Path-dan-Query-Parameter/src/components/MovieItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function MovieItem({ id, title, backdropPath, overview }) {
+    return (
+        <article>
+            <img src={backdropPath} alt={title} />
+            <h3>{title}</h3>
+            <p>{overview}</p>
+        </article>
+    );
+}
+
+MovieItem.propTypes = {
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    backdropPath: PropTypes.string.isRequired,
+    overview: PropTypes.string.isRequired
+};
+
+export default MovieItem;
