73ef0100e8e0b146225f8c1c7f9384561aaba19a
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-15 00:08:41 +0700

    Path & Query Parameter

--- ReactRouter/Path-dan-Query-Parameter/src/pages/SearchPage.js
+++ ReactRouter/Path-dan-Query-Parameter/src/pages/SearchPage.js
@@ -2,13 +2,26 @@ import React from 'react';
 import SearchBar from '../components/SearchBar';
 import MovieList from '../components/MovieList';
 import { searchMovies } from '../utils/data';
+import { useSearchParams } from 'react-router-dom';
+
+function SearchPageWrapper() {
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const title = searchParams.get('title');
+
+    function changeSearchParams(keyword) {
+        setSearchParams({ title: keyword });
+    }
+
+    return <SearchPage onSearch={changeSearchParams} activeKeyword={title} />;
+}
 
 class SearchPage extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            foundedMovies: []
+            foundedMovies: props.activeKeyword ? searchMovies(props.activeKeyword) : []
         };
 
         this.onSearch = this.onSearch.bind(this);
@@ -20,17 +33,19 @@ class SearchPage extends React.Component {
                 foundedMovies: searchMovies(keyword)
             };
         });
+
+        this.props.onSearch(keyword);
     }
 
     render() {
         return (
             <section>
                 <h2>Search Movie</h2>
-                <SearchBar search={this.onSearch} />
+                <SearchBar search={this.onSearch} defaultKeyword={this.props.activeKeyword} />
                 <MovieList movies={this.state.foundedMovies} />
             </section>
         );
     }
 }
 
-export default SearchPage;
+export default SearchPageWrapper;
