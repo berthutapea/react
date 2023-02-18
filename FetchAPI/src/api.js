c656f6be7f0b10032de129327dd8dd1f28169fe3
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-18 21:06:23 +0700

    Api

---
+++ FetchAPI/src/api.js
@@ -0,0 +1,39 @@
+/**
+ * Data restoran diambil dari RESTful API:
+ *  https://restaurant-api.dicoding.dev/
+ *
+ * Silakan baca dokumentasi untuk mengetahui-
+ * cara penggunaannya.
+ */
+
+async function getRestaurants() {
+    const response = await fetch('https://restaurant-api.dicoding.dev/list');
+    const responseJson = await response.json();
+    return responseJson.restaurants;
+}
+
+// async function getRestaurant(id) {
+//     const response = await fetch(`https://restaurant-api.dicoding.dev/detail/${id}`);
+//     const responseJson = await response.json();
+//     return responseJson.restaurant;
+// }
+
+// async function postRestaurantReview(id, name, review) {
+//     const response = await fetch('https://restaurant-api.dicoding.dev/review', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({
+//             id,
+//             name,
+//             review
+//         })
+//     });
+
+//     const responseJson = await response.json();
+
+//     return responseJson.customerReviews;
+// }
+
+export { getRestaurant, getRestaurants, postRestaurantReview };
