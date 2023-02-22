25ecc05b203ffa3f543381e14e939c0487386987
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-23 03:41:45 +0700

    style

--- ReactHooks/latihan-useState()/src/components/ToggleLamp.js
+++ ReactHooks/latihan-useState()/src/components/ToggleLamp.js
@@ -43,20 +43,20 @@ import { FaLightbulb, FaRegLightbulb } from 'react-icons/fa';
 
 // implementasi useState dari beljar react hooks 
 function ToggleLampFunc() {
-  const [lamp, setLamp] = React.useState('off');
+    const [lamp, setLamp] = React.useState('off');
 
-  const toggleLamp = () => {
-    setLamp((prevLamp) => {
-      return prevLamp === 'off' ? 'on' : 'off';
-    });
-  };
+    const toggleLamp = () => {
+        setLamp((prevLamp) => {
+            return prevLamp === 'off' ? 'on' : 'off';
+        });
+    };
 
-  return (
-    <div className={lamp}>
-      <button onClick={toggleLamp}>{lamp === 'on' ? <FaLightbulb /> : <FaRegLightbulb />}</button>
-      <h3>hooks keren!</h3>
-    </div>
-  );
+    return (
+        <div className={lamp}>
+            <button onClick={toggleLamp}>{lamp === 'on' ? <FaLightbulb /> : <FaRegLightbulb />}</button>
+            <h3>hooks keren!</h3>
+        </div>
+    );
 }
 
 // export default ToggleLamp;
