a8cd098b57fd1e3219fbb95c092f1b30b686379c
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-23 03:38:54 +0700

    Todo

---
+++ ReactHooks/latihan-useState()/src/components/Todo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+/**
+ * @todo
+ * Lengkapi komponen "Todo" sesuai dengan kriteri berikut:
+ *  1. Dapat menambahkan to-do.
+ *  2. Dapat menampilkan daftar to-do.
+ *  3. Dapat menghapus daftar to-do.
+ */
+
+function Todo() {
+  return (
+    <div>
+      <p>Lihat instruksi pada komponen ini</p>
+    </div>
+  );
+}
+
+export default Todo;
