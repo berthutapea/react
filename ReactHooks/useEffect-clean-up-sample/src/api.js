e531f12fc66f58af157f811ff93dd22ea3075fc0
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-23 05:05:54 +0700

    api

---
+++ ReactHooks/useEffect-clean-up-sample/src/api.js
@@ -0,0 +1,17 @@
+function subscribeGitHubProfile(username, listener) {
+    console.count(`subscribe for ${username}`);
+
+    // mock "subscription" ...
+    const intervalId = setInterval(async () => {
+        console.count(`listener for ${username} called`);
+        const response = await fetch(`https://api.github.com/users/${username}`);
+        const profile = await response.json();
+    }, 3000);
+
+    return () => {
+        console.count(`unsubscribe for ${username}`);
+        clearInterval(intervalId);
+    };
+}
+
+export { subscribeGitHubProfile };
