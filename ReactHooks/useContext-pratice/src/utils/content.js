fe83c5d69e4f11ab2b2de52dc85f9efc3721b906
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-23 14:17:44 +0700

    content

---
+++ ReactHooks/useContext-pratice/src/utils/content.js
@@ -0,0 +1,48 @@
+const home = {
+    id: {
+        header: 'React',
+        subheader: 'Library JavaScript untuk membangun antarmuka pengguna',
+        paragraph:
+            'React adalah library JavaScript untuk membangun antarmuka pengguna (user interface). React telah dirancang dari awal untuk adopsi bertahap, dan anda dapat menggunakan sedikit atau sebanyak yang diperlukan. Baik Anda ingin mencicipi menggunakan React, menambahkan beberapa interaktivitas ke laman HTML sederhana, atau memulai aplikasi React yang kompleks, tautan di bagian ini akan membantu Anda untuk memulai.'
+    },
+    en: {
+        header: 'React',
+        subheader: 'A JavaScript library for building user interfaces',
+        paragraph:
+            'React is a JavaScript library for building user interfaces. React has been designed from the start for gradual adoption, and you can use as little or as much React as you need. Whether you want to get a taste of React, add some interactivity to a simple HTML page, or start a complex React-powered app, the links in this section will help you get started.    '
+    }
+};
+
+const tutorial = {
+    id: [
+        'Sebelum Kita Memulai Tutorial',
+        'Mempersiapkan Tutorial',
+        'Menyelesaikan Permainan',
+        'Menambahkan Penjelajahan Waktu',
+        'Ikhtisar'
+    ],
+    en: [
+        'Before We Start the Tutorial',
+        'Setup for the Tutorial',
+        'Completing the Game',
+        'Adding Time Travel',
+        'Overview'
+    ]
+};
+
+const community = {
+    id: {
+        header: 'Di mana untuk Mendapat Dukungan',
+        subheader: 'React memiliki komunitas jutaan pengembang',
+        paragraph:
+            'Di halaman ini kami telah mencantumkan beberapa komunitas terkait React yang dapat Anda ikuti; lihat halaman lain di bagian ini untuk materi pembelajaran online dan tatap muka tambahan. Sebelum berpartisipasi dalam komunitas React, harap baca Kode Etik kami. Kami telah mengadopsi Kovenan Kontributor dan kami berharap semua anggota komunitas mematuhi pedoman di dalamnya.'
+    },
+    en: {
+        header: 'Where To Get Support',
+        subheader: 'React has a community of millions of developers.',
+        paragraph:
+            'On this page we’ve listed some React-related communities that you can be a part of; see the other pages in this section for additional online and in-person learning materials. Before participating in React’s communities, please read our Code of Conduct. We have adopted the Contributor Covenant and we expect that all community members adhere to the guidelines within.'
+    }
+};
+
+export { home, tutorial, community };
