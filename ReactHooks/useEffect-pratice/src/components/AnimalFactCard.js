efdcfb9e65bb4006b0eddfed7cc56c055dcf7107
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-23 11:57:04 +0700

    document.title

--- ReactHooks/useEffect-pratice/src/components/AnimalFactCard.js
+++ ReactHooks/useEffect-pratice/src/components/AnimalFactCard.js
@@ -39,6 +39,16 @@ function AnimalFactCard({ animal }) {
         };
     }, [animal]);
 
+    const factChangeHandler = (event) => {
+        setFact(event.target.value);
+    };
+
+    React.useEffect(() => {
+        if (fact !== null) {
+            document.title = fact;
+        }
+    }, [fact]);
+
     return (
         <section>
             <Row label="Image">
@@ -48,7 +58,9 @@ function AnimalFactCard({ animal }) {
                     <img src={image} alt={fact} />
                 )}
             </Row>
-            <Row label="Fact">{fact === null ? <p>Loading fact ...</p> : <p>{fact}</p>}</Row>
+            <Row label="Fact">
+                {fact === null ? <p>Loading fact ...</p> : <textarea value={fact} onChange={factChangeHandler} />}
+            </Row>
         </section>
     );
 }
