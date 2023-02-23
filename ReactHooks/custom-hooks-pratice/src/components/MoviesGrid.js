a0b4ec0c8264c83623caf7bd54f53cb6af99c700
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-24 04:36:46 +0700

    Logika Yang sudah diubah ke hooks

--- ReactHooks/custom-hooks-pratice/src/components/MoviesGrid.js
+++ ReactHooks/custom-hooks-pratice/src/components/MoviesGrid.js
@@ -1,33 +1,37 @@
 // Logika Yang sudah diubah ke hooks
+
 import React from 'react';
 import useMovies from '../hooks/useMovies';
 
-function MoviesList() {
-    const [movies, loading] = useMovies();
+function MoviesGrid() {
+  const [movies, loading] = useMovies();
 
-    if (loading) {
-        return (
-            <div className="movies-list">
-                <p>Loading ....</p>
-            </div>
-        );
-    }
-
+  if (loading) {
     return (
-        <ul className="movies-list">
-            {movies.map((movie) => {
-                return (
-                    <li key={movie.id} className="movies-list-item">
-                        <h1>{movie.title}</h1>
-                        <p>{movie.overview}</p>
-                    </li>
-                );
-            })}
-        </ul>
+      <div className="movies-grid">
+        <p>Loading ....</p>
+      </div>
     );
+  }
+
+  return (
+    <div className="movies-grid">
+      {movies.map((movie) => {
+        return (
+          <div key={movie.id} className="movies-grid-item">
+            <img src={movie.poster} alt={movie.title} />
+            <div className="movie-grid-item--info">
+              <h1>{movie.title}</h1>
+              <p>{movie.overview}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default MoviesList;
+export default MoviesGrid;
 
 
 
